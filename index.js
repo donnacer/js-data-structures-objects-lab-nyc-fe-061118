87840cf649a635e3c2a3f4ee04b19a769648d0ff
Donna Cerrio <denique86@yahoo.com>
2018-07-24 00:14:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let driver = {};
+
+
+function updateDriverWithKeyAndValue (driver, key, value){
+ let copyOfDriver = {...driver};
+ copyOfDriver[key] = value;
+ return copyOfDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+driver[key] = value;
+ return driver;
+}
+
+ function deleteFromDriverByKey(driver,key){
+   let copy = {...driver};
+ delete copy{key};
+ return copy;
+ }
+ 
+function destructivelyDeleteFromDriverByKeyAndValue(driver,key)
+
+return driver
+)
+  
